d0bd6f8ed48d48381de5a4a4b5e09e090b103721
BlackieZ <zouwanlong@foxmail.com>
2018-12-12 19:34:28 +0800

    delete

--- SOA/src/main/java/com/blackie/soa/dal/test/read/custom/UserTestReadDao.java
+++
@@ -1,6 +0,0 @@
-package com.blackie.soa.dal.test.read.custom;
-
-import com.blackie.soa.dal.test.read.mbg.MBGUserTestReadDao;
-
-public interface UserTestReadDao extends MBGUserTestReadDao {
-}
--- SOA/src/main/java/com/blackie/soa/dal/test/write/custom/UserTestWriteDao.java
+++
@@ -1,6 +0,0 @@
-package com.blackie.soa.dal.test.write.custom;
-
-import com.blackie.soa.dal.test.write.mbg.MBGUserTestWriteDao;
-
-public interface UserTestWriteDao extends MBGUserTestWriteDao {
-}
